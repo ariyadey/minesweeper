1105be540ee71a589353131e7b760a0a213fd660
ariya-mms <ariya.mms@gmail.com>
2020-12-12 23:17:42 +0330

    Organize the src hierarchy - Refactor

--- Minesweeper/task/src/App.js
+++ Minesweeper/task/src/App.js
@@ -1,75 +1,9 @@
 import React from "react";
 import "./App.css";
+import Minesweeper from "./Minesweeper"
 
-//todo
-function Button(props) {
-    return <button className={"cell"}>
-        {props.value}
-    </button>;
-}
-
-//todo
-function Row(props) {
-    return <div className={"row"}>
-        {props.rowArr.map((element, index) =>
-            <Button
-                key={`r${props.rowNum}_c${index}`}
-                value={element}
-            />)}
-    </div>;
-}
-
-//todo
-function Field(props) {
-    return <div className={"field"}>
-        {props.rowsArr.map((rowArr, rowNum) =>
-            <Row
-                key={rowNum}
-                rowNum={rowNum}
-                rowArr={rowArr}
-            />)}
-    </div>;
-}
-
-//todo
-class Minesweeper extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        const rowSize = 9, colSize = 8;
-        this.state = {
-            boardMatrix: Array(rowSize).fill(Array(colSize).fill())
-        }
-    }
-
-    render() {
-        return <div className={"game"}>
-            <Field
-                rowsArr={this.state.boardMatrix}
-            />
-        </div>;
-    }
-}
+export default App;
 
 function App() {
     return <Minesweeper/>;
-    // render() {
-    //     return <div className="App">
-    //         <header className="App-header">
-    //             <img src={logo} className="App-logo" alt="logo"/>
-    //             <p>
-    //                 Minesweeper is loading...
-    //             </p>
-    //             {/*<a*/}
-    //             {/*  className="App-link"*/}
-    //             {/*  href="https://reactjs.org"*/}
-    //             {/*  target="_blank"*/}
-    //             {/*  rel="noopener noreferrer"*/}
-    //             {/*>*/}
-    //             {/*  Learn React*/}
-    //             {/*</a>*/}
-    //         </header>
-    //     </div>;
-    // }
 }
-
-export default App;
---
+++ Minesweeper/task/src/Minesweeper.js
@@ -0,0 +1,73 @@
+import logo from "./asset/logo.svg";
+
+export default Minesweeper;
+
+//todo
+function Minesweeper() {
+    return <div className={"game"}>
+        <ControlPanel/>
+        <Field
+            // rowsArr={this.state.boardMatrix}
+            rowsArr={Array(9).fill(Array(8).fill(null))}
+        />
+    </div>;
+}
+
+function ControlPanel() {
+    return <div className={"control-panel"}>
+        <BrandPanel/>
+        <StatusPanel/>
+    </div>;
+}
+
+function Field(props) {
+    return <div className={"field"}>
+        {props.rowsArr.map((rowArr, rowNum) =>
+            <Row
+                key={rowNum}
+                rowNum={rowNum}
+                rowArr={rowArr}
+            />)}
+    </div>;
+}
+
+function StatusPanel() {
+    return <div className={"statusPanel"}>
+        <FlagsCounter/>
+        <Reset/>
+        <Timer/>
+    </div>
+}
+
+function BrandPanel() {
+    return <div className={"logo-panel"}>
+        <p className={"app-name"}>Minesweeper</p>
+        <img src={logo} className="app-logo" alt="logo" />
+    </div>;
+}
+
+function Cell(props) {
+    return <button className={"cell"}>
+        {props.value}
+    </button>;
+}
+
+//todo Should it know its row number?
+function Row(props) {
+    return <div className={"row"}>
+        {props.rowArr.map((element, index) =>
+            <Cell
+                key={`r${props.rowNum}_c${index}`}
+                value={element}
+            />)}
+    </div>;
+}
+
+function FlagsCounter() {
+}
+
+function Reset() {
+}
+
+function Timer() {
+}
