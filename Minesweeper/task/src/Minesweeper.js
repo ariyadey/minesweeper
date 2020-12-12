c50017665ddfc6e029d7054c79e545aadd7492ed
ariya-mms <ariya.mms@gmail.com>
2020-12-13 02:54:47 +0330

    Refactor Minesweeper.js

--- Minesweeper/task/src/Minesweeper.js
+++ Minesweeper/task/src/Minesweeper.js
@@ -4,7 +4,6 @@ import logo from "./asset/logo.svg";
 
 export default Minesweeper;
 
-//todo
 function Minesweeper() {
     return <div className={"minesweeper"}>
         <ControlPanel
@@ -15,6 +14,50 @@ function Minesweeper() {
             rowsArr={Array(9).fill(Array(8).fill(null))}
         />
     </div>;
+}
+
+function ControlPanel(props) {
+    return <div className={"control-panel"}>
+        <LogoPanel/>
+        <StatusPanel
+            flagsNum={props.flagsNum}
+            timeElapsed={props.timeElapsed}
+        />
+    </div>;
+}
+
+function LogoPanel() {
+    return <div className={"logo-panel"}>
+        <p className={"app-name"}>Minesweeper</p>
+        <img src={logo} className="app-logo" alt="logo" />
+    </div>;
+}
+
+function StatusPanel(props) {
+    return <div className={"status-panel"}>
+        <FlagsCounter
+            flagsNum={props.flagsNum}
+        />
+        <button
+            className={"reset"}>Reset
+        </button>
+        <Timer
+            timeElapsed={props.timeElapsed}
+        />
+    </div>;
+}
+
+//todo consider merging this method with timer
+function FlagsCounter(props) {
+    return <p className={"flags-counter"}>
+        {props.flagsNum}
+    </p>;
+}
+
+function Timer(props) {
+    return <p className={"timer"}>
+        {props.timeElapsed}
+    </p>;
 }
 
 function Field(props) {
@@ -42,48 +85,4 @@ function Cell(props) {
     return <button className={"cell"}>
         {props.value}
     </button>;
-}
-
-function ControlPanel(props) {
-    return <div className={"control-panel"}>
-        <LogoPanel/>
-        <StatusPanel
-            flagsNum={props.flagsNum}
-            timeElapsed={props.timeElapsed}
-        />
-    </div>;
-}
-
-function LogoPanel() {
-    return <div className={"logo-panel"}>
-        <p className={"app-name"}>Minesweeper</p>
-        <img src={logo} className="app-logo" alt="logo" />
-    </div>;
-}
-
-function StatusPanel(props) {
-    return <div className={"statusPanel"}>
-        <FlagsCounter
-            flagsNum={props.flagsNum}
-        />
-        <button
-            className={"reset"}>Reset
-        </button>
-        <Timer
-            timeElapsed={props.timeElapsed}
-        />
-    </div>;
-}
-
-//todo consider merging this method with timer
-function FlagsCounter(props) {
-    return <p className={"flags-counter"}>
-        {props.flagsNum}
-    </p>;
-}
-
-function Timer(props) {
-    return <p className={"timer"}>
-        {props.timeElapsed}
-    </p>;
 }
